a93c4cdc32ca4fb0eeff59887ed6410a4e6d4a53
bindglam <woobeen0516@gmail.com>
2026-03-07 17:47:37 +0900

    chore: remove unused dependency

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -9,7 +9,6 @@ dependencies {
     implementation(project(":api"))
     compileOnly("com.github.bindglam:ConfigLib:1.0.0")
     compileOnly("com.github.bindglam:DatabaseLib:1.0.4")
-    compileOnly("net.milkbowl.vault:VaultUnlockedAPI:2.16")
     compileOnly("org.semver4j:semver4j:6.0.0")
 }
 
